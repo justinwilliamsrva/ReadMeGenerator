c9c92c7b993fb8862bf09084ac316c1c85511aba
justinwilliamsrva <justin.williams2@me.com>
2020-07-23 14:52:51 -0400

    Create a title and description

---
+++ index.js
@@ -0,0 +1,33 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What the title of your project?",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "Please give a description of the project",
+            name: "description",
+        },
+        // {
+        //     type: "input",
+        //     message: "Please give a description of the project",
+        //     name: "description",
+        // },
+    ])
+    .then(function (response) {
+        let readme = `# ${response.title} \n ## Description \n ${response.description}`;
+        // let description = `# Description \n ${response.password}`;
+
+        fs.writeFile("README1.md", readme, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("success");
+        });
+    });
