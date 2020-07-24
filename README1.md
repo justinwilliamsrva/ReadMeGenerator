b80d708b32416cc4073f1e6fc6346a56d7023dad
justinwilliamsrva <justin.williams2@me.com>
2020-07-23 21:41:52 -0400

    Attempt to create license badges

---
+++ README1.md
@@ -0,0 +1,9 @@
+# asd 
+## Description
+asd
+## Table of Contents
+* [Installation](#installation)## Installation
+asd
+## Usage
+asd
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
--- index.js
+++ index.js
@@ -10,19 +10,55 @@ inquirer
         },
         {
             type: "input",
-            message: "Please give a description of the project",
+            message: "Provide a description of the project.",
             name: "description",
         },
         {
             type: "input",
-            message: "Please give a description of the project",
-            name: "description",
+            message: "What are the steps required to install your project? ",
+            name: "install",
+        },
+        {
+            type: "input",
+            message: "Provide instructions and examples for use. ",
+            name: "usage",
+        },
+        {
+            type: "list",
+            message: "What license will you use?",
+            name: "license",
+            choices: ["MIT", "Apache", "GLPv3", "BSD"],
         },
     ])
     .then(function (response) {
-        let readme = `# ${response.title} \n ## Description \n ${response.description} \n `;
-        // let description = `# Description \n ${response.password}`;
+        let title = `# ${response.title} \n`;
+        let desc = `## Description\n${response.description}\n`;
+        let table = `## Table of Contents\n* [Installation](#installation)`;
+        let install = `## Installation\n${response.install}\n`;
+        let usage = `## Usage\n${response.usage}\n`;
+        let license = "";
+        console.log(response.license);
+        console.log(typeof response.license);
+        function licenseBadge() {
+            switch (response.license) {
+                case "MIT":
+                    license =
+                        "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+                    break;
+                case "Apache":
+                    console.log("fog");
+                    break;
+                case "GLPv3":
+                    console.log("fog");
+                    break;
+                case "BSD":
+                    console.log("fog");
+                    break;
+            }
+        }
+        licenseBadge();
 
+        let readme = title + desc + table + install + usage + license;
         fs.writeFile("README1.md", readme, function (err) {
             if (err) {
                 return console.log(err);
