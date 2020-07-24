565f84f304e24298a96929feda1af1f9d1822dd8
justinwilliamsrva <justin.williams2@me.com>
2020-07-24 09:25:52 -0400

    Added a test section

--- index.js
+++ index.js
@@ -34,18 +34,24 @@ inquirer
             message: "Provide your contribution guidelines.",
             name: "contribute",
         },
+        {
+            type: "input",
+            message: "Provide any tests for the website",
+            name: "tests",
+        },
     ])
     .then(function (response) {
         let title = `# ${response.title} \n`;
         let desc = `## Description\n${response.description}\n`;
-        let table = `## Table of Contents\n* [Installation](#installation)\n`;
+        let table = `## Table of Contents\n* [Installation](#installation)\n* [Usage](#usage)\n* [License](#license)\n* [Contribution Guidelines](#contribution guidelines)\n`;
         let install = `## Installation\n${response.install}\n`;
         let usage = `## Usage\n${response.usage}\n`;
         let license = "";
         licenseBadge();
         let contribute = `## Contribution Guidelines\n${response.contribute}\n`;
+        let test = `## Tests\n${response.tests}\n`;
 
-        let readme = title + desc + table + install + usage + license + contribute;
+        let readme = title + desc + table + install + usage + license + contribute + test;
         fs.writeFile("README1.md", readme, function (err) {
             if (err) {
                 return console.log(err);
@@ -58,19 +64,19 @@ inquirer
             switch (response.license) {
                 case "MIT":
                     license =
-                        "##License\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n";
+                        "## License\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n";
                     break;
                 case "Apache":
                     license =
-                        "##License\n[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n";
+                        "## License\n[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)\n";
                     break;
                 case "GPLv3":
                     license =
-                        "##License\n[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n";
+                        "## License\n[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)\n";
                     break;
                 case "BSD":
                     license =
-                        "##License\n[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n";
+                        "## License\n[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)\n";
             }
         }
     });
